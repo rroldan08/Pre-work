a40bee5830d5f93fa1bf579ae07e27d4f6ba588b
Roger Roldan <78450150+rroldan08@users.noreply.github.com>
2022-05-31 10:29:21 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var form =
+document.getElementById('form');
+submit.addEventListener('submit', showMessage);
+
+function showMessage() {
+    alert("Either way, I think we can get along!");
+}
+
+
+function yeahItDoes(){
+    document.getElementById("name").value = "Of course it does!";
+    
+    
+}
